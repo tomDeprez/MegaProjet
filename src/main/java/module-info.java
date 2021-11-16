4097eec25a5463eebbc34111bf740144917060e0
tom <tom_deprez@hotmail.fr>
2021-11-16 10:18:59 +0100

    V1.0 first

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module mayol.tom.megaprojet {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires eu.hansolo.tilesfx;
+
+    opens mayol.tom.megaprojet to javafx.fxml;
+    exports mayol.tom.megaprojet;
+}
