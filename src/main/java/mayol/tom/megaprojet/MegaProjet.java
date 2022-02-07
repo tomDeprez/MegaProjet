ef3a0869e4fe096db649b821c2ffe74c4905bad4
Mayol <mayolboucaud@gmail.com>
2022-02-07 14:06:01 +0100

    set connexion page default

--- src/main/java/mayol/tom/megaprojet/MegaProjet.java
+++ src/main/java/mayol/tom/megaprojet/MegaProjet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MegaProjet extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MegaProjet.class.getResource("home-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MegaProjet.class.getResource("Connexion.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 400);
         stage.setTitle("Petit ours brun!");
         stage.setScene(scene);
