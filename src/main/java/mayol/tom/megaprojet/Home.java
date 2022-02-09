08f372e79d68e5ad5c945650b1deb2ca42aa9539
Mayol <mayolboucaud@gmail.com>
2022-02-09 15:17:54 +0100

    switch page home et profil

--- src/main/java/mayol/tom/megaprojet/Home.java
+++ src/main/java/mayol/tom/megaprojet/Home.java
@@ -1,11 +1,54 @@
 package mayol.tom.megaprojet;
 
-import javafx.scene.layout.BorderPane;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Home {
 
-    public void changePage()
-    {
+    @FXML
+    public Button btnHome;
 
+    @FXML
+    public Button btnTrading;
+
+    @FXML
+    public Button btnAutres;
+
+    @FXML
+    public Button btnProfil;
+
+    private Stage stage;
+    private Scene scene;
+
+    @FXML
+    protected void showProfil(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("profil-view.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    @FXML
+    protected void showHome(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("home-view.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
 }
--- src/main/java/mayol/tom/megaprojet/Profil.java
+++ src/main/java/mayol/tom/megaprojet/Profil.java
@@ -1,5 +1,44 @@
 package mayol.tom.megaprojet;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Profil {
 
+    public Button btnHome;
+    public Button btnTrading;
+    public Button btnAutres;
+    public Button btnProfil;
+
+    private Stage stage;
+    private Scene scene;
+
+
+    @FXML
+    protected void showProfil(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("profil-view.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void showHome(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("home-view.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
